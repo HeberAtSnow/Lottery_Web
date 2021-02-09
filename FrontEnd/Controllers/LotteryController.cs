cf0341e5e2ad4d113f3845424321dd2d6ff3a578
Maksad Annamuradov <maksad.annamuradov@students.snow.edu>
2021-02-09 10:55:27 -0700

    Added basic main and lottery razor pages

---
+++ FrontEnd/Controllers/LotteryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FrontEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LotteryController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<LotteryController> _logger;
+
+        public LotteryController(ILogger<LotteryController> logger)
+        {
+            _logger = logger;
+        }
+
+    
+    }
+}
--- FrontEnd/Startup.cs
+++ FrontEnd/Startup.cs
@@ -26,29 +26,25 @@ namespace FrontEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddRazorPages();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FrontEnd", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FrontEnd v1"));
-            }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
 
             app.UseEndpoints(endpoints =>
             {
