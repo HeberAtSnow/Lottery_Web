aea700aa3da4a6cc2d1f1e47310d416e8daef238
Ofnir01 <emmanuel.torresamaya@gmail.com>
2021-02-09 21:58:33 -0700

    I am able to display last ticket bought

--- FrontEnd/Pages/Store.cshtml.cs
+++ FrontEnd/Pages/Store.cshtml.cs
@@ -11,32 +11,38 @@ namespace FrontEnd.Pages
     public class StoreModel : PageModel
     {
         private IMemoryCache _cache;
-        private string cacheKey = "Selection";
-        private string cacheValue;
-        public int[] Ticket;
-        public string Selection => cacheValue ?? "";
+        private const string cacheSelectionKey = "Selection";
+        private const string cacheLastTicketKey = "LastTicket";
+        private const string cacheRecentPurchaseKey = "RecentPurchase";
+        private string cacheSelectionValue;
+        private int[] _lastTicket;
+        private bool recentPurcahse;
+        public string Selection => cacheSelectionValue ?? "";
+        public int[] LastTicket => _lastTicket ?? (_lastTicket = new int[6]);
+        public bool RecentPurchase => recentPurcahse;
 
         public StoreModel(IMemoryCache cache)
         {
-            Ticket = new int[6];
             _cache = cache;
         }
 
         public void OnGet()
         {
-            _cache.TryGetValue(cacheKey, out cacheValue);
+            _cache.TryGetValue(cacheRecentPurchaseKey, out recentPurcahse);
+            _cache.TryGetValue(cacheLastTicketKey, out _lastTicket);
+            _cache.TryGetValue(cacheSelectionKey, out cacheSelectionValue);
         }
 
         public IActionResult OnPostQuickPick()
         {
-            cacheValue = "QuickPick";
-            _cache.Set(cacheKey, cacheValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
+            cacheSelectionValue = "QuickPick";
+            _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
             return RedirectToPage();
         }
         public IActionResult OnPostNumberPick()
         {
-            cacheValue = "NumberPick";
-            _cache.Set(cacheKey, cacheValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
+            cacheSelectionValue = "NumberPick";
+            _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
             return RedirectToPage();
         }
 
@@ -47,6 +53,8 @@ namespace FrontEnd.Pages
 
         public IActionResult OnPostNumberPickPurchase(int [] ticket)
         {
+            _cache.Set(cacheRecentPurchaseKey, true);
+            _cache.Set(cacheLastTicketKey, ticket);
             return RedirectToPage();
         }
     }
