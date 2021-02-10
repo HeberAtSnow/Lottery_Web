46876083af5978124285342d58c61c1678357ef9
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2021-02-09 18:55:02 -0700

    worked on css

--- FrontEnd/Startup.cs
+++ FrontEnd/Startup.cs
@@ -26,7 +26,7 @@ namespace FrontEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddRazorPages();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -52,6 +52,7 @@ namespace FrontEnd
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllers();
             });
         }
