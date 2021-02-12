5188ad6701abf07254bdfa22b0be81689ff106ab
Chance Young <ChanceTheHackerv2@gmail.com>
2021-02-11 18:51:50 -0700

    2 hours up, need to implement random ticket generator to finish display.

--- FrontEnd/Pages/Store.cshtml.cs
+++ FrontEnd/Pages/Store.cshtml.cs
@@ -16,44 +16,80 @@ namespace FrontEnd.Pages
         private const string cacheSelectionKey = "Selection";
         private const string cacheLastTicketKey = "LastTicket";
         private const string cacheRecentPurchaseKey = "RecentPurchase";
+        private const string cacheNameKey = "PlayerName";
+        private const string cacheBoughtKey = "BuySuccess";
+        private string cacheName;
         private string cacheSelectionValue;
         private int[] _lastTicket;
-        private bool recentPurcahse;
+        private bool recentPurchase;
+        private bool BoughtVal;
+
+
+        [BindProperty]
+        public string name { get; set; }
+        public bool boughtVal => BoughtVal; 
         public string Selection => cacheSelectionValue ?? "";
         public int[] LastTicket => _lastTicket ?? (_lastTicket = new int[6]);
-        public bool RecentPurchase => recentPurcahse;
+        public bool RecentPurchase => recentPurchase;
 
         public StoreModel(IMemoryCache cache,LotteryProgram prog)
         {
             _cache = cache;
             lp = prog;
+          
         }
 
         public void OnGet()
         {
-            _cache.TryGetValue(cacheRecentPurchaseKey, out recentPurcahse);
+            _cache.TryGetValue(cacheRecentPurchaseKey, out recentPurchase);
             _cache.TryGetValue(cacheLastTicketKey, out _lastTicket);
             _cache.TryGetValue(cacheSelectionKey, out cacheSelectionValue);
+            _cache.TryGetValue(cacheNameKey, out cacheName);
+            _cache.TryGetValue(cacheBoughtKey, out BoughtVal);
         }
 
         public IActionResult OnPostQuickPick()
         {
+            if (name != null)
+            { 
             cacheSelectionValue = "QuickPick";
+            cacheName = name;
+            _cache.Set(cacheNameKey, cacheName, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(90)));
             _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
             return RedirectToPage();
+            }
+            else
+            {
+                return RedirectToPage();
+            }
         }
         public IActionResult OnPostNumberPick()
         {
-            cacheSelectionValue = "NumberPick";
-            _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
-            return RedirectToPage();
+            if(name != null)
+            {
+                cacheSelectionValue = "NumberPick";
+                _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
+                return RedirectToPage();
+            }
+            else
+            {
+                return RedirectToPage();
+            }
         }
 
         public IActionResult OnPostQuickPickPurchase(int numTickets)
         {
-            //START HERE
-            //lp.lv.SellQuickTickets(____playername_____, ____qty____);
-            return RedirectToPage();
+            name = _cache.Get(cacheNameKey).ToString();
+            if (name != null && numTickets >= 1 && numTickets <=1000)
+            { 
+                _cache.Set(cacheBoughtKey, lp.lv.SellQuickTickets(name, numTickets));
+                return RedirectToPage();
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+                
         }
 
         public IActionResult OnPostNumberPickPurchase(int [] ticket)
