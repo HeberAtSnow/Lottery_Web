5fb06f8ee55717c587d31e4d9d3589f9f48c916d
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2021-02-18 16:59:20 -0700

    I have been working on the settings page

--- FrontEnd/Pages/Settings.cshtml.cs
+++ FrontEnd/Pages/Settings.cshtml.cs
@@ -2,15 +2,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassLib;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FrontEnd.Pages
 {
+
     public class SettingsModel : PageModel
     {
+        private LotteryProgram lotteryProgram;
+        public IEnumerable<LotteryTicket> lotteryResults;
+
+        public SettingsModel(LotteryProgram prog)
+        {
+            lotteryProgram = prog;
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPostResetLottery()
+        {
+            lotteryProgram.ResetPeriod();
+            return Page();
+        }
+
+        public IActionResult OnPostProcessDrawing()
+        {
+            if (lotteryProgram.ClosePeriodSales())
+            {
+
+            }
+            if (lotteryProgram.p.DrawWinningTicket())
+            {
+
+            }
+            try
+            {
+                lotteryProgram.p.ComputeWinners();
+            }
+            catch
+            {
+
+            }
+            lotteryResults = lotteryProgram.p.ResultsByWinLevel();
+            return Page();
+        }
+        public IActionResult OnPostLotteryResults()
+        {
+            lotteryResults = lotteryProgram.p.ResultsByWinLevel();
+            return Page();
+        }
+
+        public IActionResult OnPostHistoricalStats()
+        {
+
+            return Page();
         }
     }
 }
