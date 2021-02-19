448238afecfd8688928a8de98ed47df4f9e79039
Rusty Hermansen <dr.delight@gmail.com>
2021-02-19 01:19:36 -0700

    Toyed around with a few other little things, a lot of the settings stuff hasn't been implimented yet so I sort of got the reset setup (superficially at least).

--- FrontEnd/Pages/Settings.cshtml.cs
+++ FrontEnd/Pages/Settings.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassLib;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,22 @@ namespace FrontEnd.Pages
 {
     public class SettingsModel : PageModel
     {
+        LotteryProgram lp;
+        //lotteryvendor.lotteryprogram
+        //lotteryprogram has a lottery period
+        public SettingsModel(LotteryProgram lotteryProgram)
+        {
+            lp = lotteryProgram;
+        }
         public void OnGet()
         {
         }
+        public void OnPostResetLottery()
+        {
+            lp.ResetPeriod();
+        }
+        //draw winning numbers
+        //current lottery results
+        //all lottery statistics
     }
 }
