5708327020150cfc75189b65f11548d03ebb19ef
Rusty Hermansen <dr.delight@gmail.com>
2021-02-18 22:16:55 -0700

    Got it mostly working. I do have a little bug I'm trying to work out. I also added some extra styling.

--- FrontEnd/Pages/Store.cshtml.cs
+++ FrontEnd/Pages/Store.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using ClassLib;
@@ -11,73 +12,66 @@ namespace FrontEnd.Pages
 {
     public class StoreModel : PageModel
     {
-        private IMemoryCache _cache;
         private LotteryProgram lp;
         public IEnumerable<LotteryTicket> PurchasedTickets;
+        [Required]
         public string PlayerNombre;
         public int NumQuickPicks;
-        private const string cacheSelectionKey = "Selection";
-        private const string cacheLastTicketKey = "LastTicket";
-        private const string cacheRecentPurchaseKey = "RecentPurchase";
-        private string cacheSelectionValue;
         private int[] _lastTicket;
-        private bool recentPurcahse;
-        public string Selection => cacheSelectionValue ?? "";
+        private bool recentPurchase;
+        public string Selection;
         public int[] LastTicket => _lastTicket ?? (_lastTicket = new int[6]);
-        public bool RecentPurchase => recentPurcahse;
+        public bool RecentPurchase => recentPurchase;
 
         public StoreModel(IMemoryCache cache,LotteryProgram prog)
         {
-            _cache = cache;
             lp = prog;
         }
 
         public void OnGet()
         {
-            _cache.TryGetValue(cacheRecentPurchaseKey, out recentPurcahse);
-            _cache.TryGetValue(cacheLastTicketKey, out _lastTicket);
-            _cache.TryGetValue(cacheSelectionKey, out cacheSelectionValue);
+            
         }
 
         public IActionResult OnPostQuickPick(string name)
         {
             PlayerNombre = name;
-            cacheSelectionValue = "QuickPick";
-            _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
+            Selection = "QuickPick";
+            PurchasedTickets = lp.p.ResultsByPlayer(name);
             return Page();
         }
-        public IActionResult OnPostNumberPick()
+        public IActionResult OnPostNumberPick(string name)
         {
-            cacheSelectionValue = "NumberPick";
-            _cache.Set(cacheSelectionKey, cacheSelectionValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(10)));
+            PlayerNombre = name;
+            Selection = "NumberPick";
+            PurchasedTickets = lp.p.ResultsByPlayer(name);
             return Page();
         }
 
         public IActionResult OnPostQuickPickPurchase(string name,int numTickets)
         {
-            //START HERE
-            //lp.lv.SellQuickTickets(____playername_____, ____qty____);
-
-            //TODO: need to read the html variable "name"
-            //      and save it to Model's private string playerNombre
+            
             //      ensure not null
             PlayerNombre = name;
+            Selection = "QuickPick";
             NumQuickPicks = numTickets;
+           
 
-            //Doh! I first tried to get just this ticket sales.  Wrong!
-            //What is needed is to get all ticket sales for this player-name
-            //PurchasedTickets = lp.lv.SellQuickTickets(name, numTickets);//TODO: replace "x" with playerNobmre
+
+            
             lp.lv.SellQuickTickets(name, numTickets);
             PurchasedTickets = lp.p.ResultsByPlayer(name);
             return Page();
         }
 
-        public IActionResult OnPostNumberPickPurchase(int [] ticket)
+        public IActionResult OnPostNumberPickPurchase(String name, int [] ticket)
         {
+            PlayerNombre = name;
+            Selection = "NumberPick";
             if (ticket.Length == 6)
             {
-                _cache.Set(cacheRecentPurchaseKey, true);
-                _cache.Set(cacheLastTicketKey, ticket);
+                lp.lv.SellTicket(name, ticket);
+                PurchasedTickets = lp.p.ResultsByPlayer(name);
             }
             return Page();
         }
