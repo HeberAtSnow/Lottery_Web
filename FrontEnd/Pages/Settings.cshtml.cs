e29bc0155ef4e86e807ac57a959e375916d7d8f3
Chance Young <ChanceTheHackerv2@gmail.com>
2021-03-03 00:09:00 -0700

    The ol college try

--- FrontEnd/Pages/Settings.cshtml.cs
+++ FrontEnd/Pages/Settings.cshtml.cs
@@ -14,18 +14,46 @@ namespace FrontEnd.Pages
         LotteryProgram lp;
         //lotteryvendor.lotteryprogram
         //lotteryprogram has a lottery period
+        public string message;
+        private bool periodClosed;
         public SettingsModel(LotteryProgram lotteryProgram)
         {
             lp = lotteryProgram;
         }
-        public void OnGet()
+
+        public void OnGetAfterClose(bool persistor)
         {
+            periodClosed = persistor;
         }
 
         public void OnPostResetLottery()
         {
-            lp.ResetPeriod();
+            try
+            {
+                if (periodClosed)
+                {
+                    message = "Lottery Period has been reset";
+                    lp.ResetPeriod();
+                }
+                else
+                {
+                    lp.ResetPeriod();
+                }
+            }
+            catch (Exception ex)
+            {
+                message = "Cannot reset Lottery Period while sales are ongoing, Please process drawing first";
+            }
         }
+        
+        public IActionResult OnPostProcessDrawing()
+        {
+            periodClosed = lp.ClosePeriodSales();
+            return RedirectToPage("./Settings", "AfterClose", new { persistor = periodClosed });
+
+        }
+
+
         //draw winning numbers
         //current lottery results
         //all lottery statistics
