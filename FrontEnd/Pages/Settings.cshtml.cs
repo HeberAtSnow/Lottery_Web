a9eaea26bc05477dcd117dc188a6841fe86ceef9
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2021-03-04 19:55:48 -0700

    logging for admin page is done

--- FrontEnd/Pages/Settings.cshtml.cs
+++ FrontEnd/Pages/Settings.cshtml.cs
@@ -18,6 +18,8 @@ namespace FrontEnd.Pages
         public IEnumerable<TicketSale> stats;
         public string error;
         public string msg = "";
+        public DateTime startTime= new DateTime();
+        public DateTime endTime = new DateTime();
         //lotteryvendor.lotteryprogram
         //lotteryprogram has a lottery period
         public SettingsModel(LotteryProgram lotteryProgram, ILogger<SettingsModel> logger )
@@ -27,7 +29,8 @@ namespace FrontEnd.Pages
         }
         public void OnGet()
         {
-            logger.LogInformation("went to Administration page {time}");
+            var time = DateTime.Now;
+            logger.LogInformation("went to Administration page {time}", time);
         }
 
         public void OnPostResetLottery()
@@ -37,6 +40,7 @@ namespace FrontEnd.Pages
                var x=lp.ResetPeriod();
                 if (x)
                     msg = "Reset the period";
+                logger.LogInformation(msg);
             }
             catch
             {
@@ -47,30 +51,62 @@ namespace FrontEnd.Pages
         }
         public IActionResult OnPostDrawLottery()
         {
+            startTime = DateTime.Now;
             logger.LogInformation("clicked DrawLottery");
             lp.ClosePeriodSales();
             lp.p.DrawWinningTicket();
             lp.p.ComputeWinners();
+            endTime = DateTime.Now;
+            var time = endTime - startTime;
+            logger.LogInformation("DrawLottery took {time} to process", time);
             return Page();
         }
         public IActionResult OnPostResultsLottery()
         {
+            logger.LogInformation("clicked Result lottery");
+            var startTime = DateTime.Now;
             var lotteryResults = lp.p.ResultsByWinLevel();
+            var endTime = DateTime.Now;
+            var eleapsed = endTime - startTime;
+            logger.LogInformation(" time to complete results of the lotter {eleapsed}", eleapsed);
+            if (lotteryResults == null)
+            {
+                logger.LogWarning("Lottery results is null");
+            }
+            else
+            {
+                logger.LogInformation("lottery count = {count}", lotteryResults.Count());
+            }
             return Page();
         }
         public IActionResult OnPostAllStats()
         {
-            try { 
+            logger.LogInformation("Clicke on get all stats");
+            try {
+                var startTime = DateTime.Now;
             stats = ls.DBStatsAllPeriods();
+                var endTime = DateTime.Now;
+                var elapsed = endTime - startTime;
+                logger.LogInformation("To get the stats took {elapsed}", elapsed);
+                if (stats == null)
+                {
+                    logger.LogWarning("Stats returned null");
+                }
+                else
+                {
+                    logger.LogInformation("stats returned {count} stats", stats.Count());
+                }
             }
             catch
             {
                 error = "failed to get history";
+                logger.LogWarning(error);
             }
             return Page();
         }
         public IActionResult OnPostBackToIndex()
         {
+            logger.LogInformation("Returning to index");
             return RedirectToPage("./Index");
         }
     }
