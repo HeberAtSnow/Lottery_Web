b127ba2f44942340ab26c907fc33875570f17635
Maksad Annamuradov <46725876+MaksadAnnamuradov@users.noreply.github.com>
2021-03-04 23:18:20 -0700

    Added logging for Store page in frontEnd

--- FrontEnd/Pages/Store.cshtml.cs
+++ FrontEnd/Pages/Store.cshtml.cs
@@ -7,12 +7,14 @@ using ClassLib;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace FrontEnd.Pages
 {
     public class StoreModel : PageModel
     {
         private LotteryProgram lp;
+        private readonly ILogger<StoreModel> logger;
         public IEnumerable<LotteryTicket> PurchasedTickets;
         [Required]
         public string PlayerNombre;
@@ -23,67 +25,103 @@ namespace FrontEnd.Pages
         public int[] LastTicket => _lastTicket ?? (_lastTicket = new int[6]);
         public bool RecentPurchase => recentPurchase;
 
-        public StoreModel(IMemoryCache cache,LotteryProgram prog)
+        public StoreModel(IMemoryCache cache,LotteryProgram prog, ILogger<StoreModel> logger)
         {
             lp = prog;
+            this.logger = logger;
         }
 
         public void OnGet()
         {
+            logger.LogInformation("User entered the lottery store page");
+        }
 
+
+        public IEnumerable<LotteryTicket> getPlayerResults(string name)
+        {
+            logger.LogInformation("user with name: {name} called on getting player results", name);
+            return lp.p.ResultsByPlayer(name);
         }
 
         public IActionResult OnPostQuickPick(string name)
         {
+            logger.LogInformation("user with name: {name} clicked on Quick Pick", name);
+
             PlayerNombre = name;
             Selection = "QuickPick";
-            PurchasedTickets = lp.p.ResultsByPlayer(name);
+            PurchasedTickets = getPlayerResults(name);
+
+            
+
             return Page();
         }
         public IActionResult OnPostNumberPick(string name)
         {
+            logger.LogInformation("user with name: {name} clicked on Number Pick", name);
+
             PlayerNombre = name;
             Selection = "NumberPick";
-            PurchasedTickets = lp.p.ResultsByPlayer(name);
+            PurchasedTickets = getPlayerResults(name);
+
+            logger.LogInformation("user with name: {name} successfuly purchased numbePick tickets", name);
+
             return Page();
         }
 
         public IActionResult OnPostQuickPickPurchase(string name,int numTickets)
         {
+            logger.LogInformation("user with name: {name} clicked on Quick Pick to purchase {numTickets} tickets", name, numTickets);
+
             try
             {
+
                 PlayerNombre = name;
                 Selection = "QuickPick";
                 NumQuickPicks = numTickets;
                 lp.lv.SellQuickTickets(name, numTickets);
+
+                logger.LogInformation("user with name: {name} successfuly purchased {numTickets} quickPick tickets", name, numTickets);
             }
             catch (Exception e)
             {
+                logger.LogInformation("user with name: {name} clicked on Quick Pick to purchase {numTickets} tickets, but request was failed", name, numTickets);
                 Console.WriteLine(e.Message);
                 return Page();
             }
-            PurchasedTickets = lp.p.ResultsByPlayer(name);
+
+            PurchasedTickets = getPlayerResults(name);
+
             return Page();
         }
 
         public IActionResult OnPostNumberPickPurchase(string name, int [] ticket)
         {
+
+            logger.LogInformation("user with name: {name} clicked on Num Pick to purchase {ticket} tickets", name, string.Join(",", ticket));
+
             try
             {
                 PlayerNombre = name;
                 Selection = "NumberPick";
                 if (ticket.Length != 6)
                 {
+                    logger.LogInformation("user with name: {name} clicked on Num Pick to purchase {ticket} tickets, but the request was more than 6 ticket numbers", name, string.Join(",", ticket));
                     throw new Exception("Ticket length is not six.");
+
                 }
                 lp.lv.SellTicket(name, ticket);
+                logger.LogInformation("user with name: {name} successfuly purchased {ticket} quickPick tickets", name, string.Join(",", ticket));
+
             }
             catch (Exception e)
             {
+                logger.LogInformation("user with name: {name} clicked on Quick Pick to purchase {tickets} tickets, but request was failed", name, string.Join(",", ticket));
                 Console.WriteLine(e.Message);
                 return Page();
             }
-            PurchasedTickets = lp.p.ResultsByPlayer(name);
+
+            getPlayerResults(name);
+
             return Page();
         }
     }
