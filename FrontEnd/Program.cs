ee8cec94d48c89f2dd26ed05c7b504c8dc1d65c6
Maksad Annamuradov <46725876+MaksadAnnamuradov@users.noreply.github.com>
2021-03-04 23:03:10 -0700

    Added serilog support and integrated it to front end app.

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace FrontEnd
 {
@@ -13,7 +14,27 @@ namespace FrontEnd
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+            try
+            {
+                Log.Information("FrontEnd is Starting.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "FrontEnd failed to start.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
