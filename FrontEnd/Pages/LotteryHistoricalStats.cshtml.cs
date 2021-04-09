8b4f76ecb6f8f11437f6f30168d43f1bcb9877e0
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2021-04-08 20:11:26 -0600

    removed logging. changed up style. didnt get historicle stats done

---
+++ FrontEnd/Pages/LotteryHistoricalStats.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Pages
+{
+    public class LotteryHistoricalStatsModel : PageModel
+    {
+        private readonly LotteryStatistics lotteryStats;
+        public IEnumerable<TicketSale> Sales { get; private set; }
+        public LotteryHistoricalStatsModel(LotteryStatistics lotteryStatistics)
+        {
+            
+            lotteryStats = lotteryStatistics;
+        }
+        public void OnGet()
+        {
+                Sales = lotteryStats.DBStatsAllPeriods();
+
+        }
+    }
+}
