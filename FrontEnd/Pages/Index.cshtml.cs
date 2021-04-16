949d4a2e39486c4fbad613bb63a992d0cd833f87
dallynowens2 <dallynowens2@gmail.com>
2021-04-15 22:29:28 -0600

    Added logging for seq and basic logging

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -4,21 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            _logger.LogInformation("Page index page was loaded");
         }
 
         public IActionResult OnPostGoToStore()
         {
+            _logger.LogInformation("Page 2 was loaded");
             return RedirectToPage("./Store");
         }
         public IActionResult OnPostGoToSettings()
         {
+            _logger.LogInformation("Page 2 was loaded");
             return RedirectToPage("./Settings");
         }
     }
