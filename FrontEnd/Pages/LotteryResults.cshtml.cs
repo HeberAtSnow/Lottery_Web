46dc333e3d5388a65ca7df78bbf9128a9b6d3b38
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2021-04-16 06:23:19 -0600

    inished logging on results

--- FrontEnd/Pages/LotteryResults.cshtml.cs
+++ FrontEnd/Pages/LotteryResults.cshtml.cs
@@ -5,23 +5,31 @@ using System.Threading.Tasks;
 using ClassLib;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
 
 namespace FrontEnd.Pages
 {
     public class LotteryResultsModel : PageModel
     {
         LotteryProgram lp { get; }
+        public ILogger Logger { get; }
         public IEnumerable<LotteryTicket> results { get; set; }
 
 
-        public LotteryResultsModel(LotteryProgram lotteryProgram )
+        public LotteryResultsModel(LotteryProgram lotteryProgram, ILogger logger )
         {
             lp = lotteryProgram;
-
+            Logger = logger;
         }
         public void OnGet()
         {
+            Logger.Information("Getting Results for the most resentlottery period");
                 results = lp.p.ResultsByWinLevel();
+            Logger.Information("count of results = {num}", results.Count());
+            if (results.Count() < 1)
+            {
+                Logger.Warning("you requested results but did not get any returned");
+            }
        
         }
     }
