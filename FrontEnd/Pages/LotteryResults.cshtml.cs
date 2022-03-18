0e05c24d7f04e8f7e7ccb1335e08ce26d9c8191e
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-03-18 00:21:27 -0600

    some changes

--- FrontEnd/Pages/LotteryResults.cshtml.cs
+++ FrontEnd/Pages/LotteryResults.cshtml.cs
@@ -11,18 +11,54 @@ namespace FrontEnd.Pages
 {
     public class LotteryResultsModel : PageModel
     {
-        private readonly ILogger<LotteryResultsModel> logger;
-
-        LotteryProgram LotteryProgram { get; }
+        LotteryProgram lp { get; }
         public IEnumerable<LotteryTicket> results { get; set; }
 
 
-        public LotteryResultsModel(LotteryProgram lotteryProgram, ILogger<LotteryResultsModel> logger)
+        public IEnumerable<LotteryTicket> WinLotteryTickets;
+        public IEnumerable<TicketSale> CurrPeriodWinTickets;
+        private readonly LotteryStatistics lotteryStatistics;
+        private readonly ILogger<LotteryResultsModel> logger;
+        public IEnumerable<TicketSale> ticketSales;
+        public IEnumerable<(int periodid, DateTime started)> LotteryPeriods;
+
+        public LotteryResultsModel(LotteryProgram lotteryProgram, LotteryStatistics _lotteryStatistics, ILogger<LotteryResultsModel> logger)
         {
-            LotteryProgram = lotteryProgram;
+            lotteryStatistics = _lotteryStatistics;
             this.logger = logger;
+            lp = lotteryProgram;
         }
-        public IActionResult OnGet()
+        public void OnGet()
+        {
+            LotteryResults();
+        }
+
+        public IActionResult LotteryResults()
+        {
+            WinLotteryTickets = lp.Period.ResultsByWinLevel();
+            LotteryPeriods = lotteryStatistics.DBPeriodsInHistory();
+            var list = LotteryPeriods.ToList();
+
+            CurrPeriodWinTickets = lotteryStatistics.DBStatsOnePeriod(list.Last().periodid);
+
+            try
+            {
+                results = lp.Period.ResultsByWinLevel();
+                logger.LogDebug("[{prefix}]: Successfully retrieved results for the current lottery period",
+                    LogPrefix.Stats);
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning("[{prefix}]: Failed to retrieve the results for the current lottery period. Reason: {ex}",
+                    LogPrefix.Stats, ex);
+
+                return RedirectToPage("./Error");
+            }
+
+        }
+        /*public IActionResult OnGet()
         {
             try
             {
@@ -39,6 +75,6 @@ namespace FrontEnd.Pages
 
                 return RedirectToPage("./Error");
             }
-        }
+        }*/
     }
 }
--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using ClassLib;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace FrontEnd
 {
@@ -15,11 +17,19 @@ namespace FrontEnd
     {
         public static void Main(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
 
             Log.Logger = new LoggerConfiguration()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearch:Url"]))
+                {
+                    AutoRegisterTemplate = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+                })
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
 
